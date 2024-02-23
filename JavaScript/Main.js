0d3342c9397c26f174c12b3b1ad93a4c5643f338
MuhammadAwaisX <160998964+MuhammadAwaisX@users.noreply.github.com>
2024-02-22 21:51:50 -0800

    Added

---
+++ JavaScript/Main.js
@@ -0,0 +1,8 @@
+// Loop through numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    // For each number, loop from 1 to 10 and print the multiplication result
+    for (lt j = 1; j <= 10; j++) {
+        // Calculate the result and print it
+        console.log(i + " x " + j + " = " + (i * j));
+    }
+}
